394cf2bedec176d0adab8b63df0753af87e565e2
MCappe <melissacappe@gmail.com>
2020-12-01 11:44:01 -0500

    gh-pages and playing around

--- src/Podcasts.js
+++ src/Podcasts.js
@@ -4,17 +4,19 @@ class Podcasts extends Component {
     render() {
         return (
             <div>
-                <ul>
-                    <li className="container">
-                        <div className="imageContainer">
-                            <img src={this.props.podImage} alt={this.props.podTitle}/>
-                        </div>
-                        <div className="podInfo">
-                            <h2>{this.props.podTitle}</h2>
-                            <p>{this.props.podDescription}</p>
-                        </div>
-                    </li>
-                </ul>
+                <section className="podSection">
+                    <ul>
+                        <li className="container">
+                            <div className="imageContainer">
+                                <img src={this.props.podImage} alt={this.props.podTitle}/>
+                            </div>
+                            <div className="podInfo">
+                                <h2>{this.props.podTitle}</h2>
+                                <p>{this.props.podDescription}</p>
+                            </div>
+                        </li>
+                    </ul>
+                </section>
             </div>
         )
     }
